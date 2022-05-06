46eb504f83697ad3b0ef5cba7f4837902e630eac
kangminlee1 <jack7853@naver.com>
2022-05-06 17:55:44 +0900

    baekjoon9012

---
+++ src/baekjoon/PSstack.java
@@ -0,0 +1,33 @@
+package baekjoon;
+import java.util.Scanner;
+import java.util.Stack;
+public class PSstack {
+	public static char[] arr;
+	public static int size=0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		int A=in.nextInt();
+		for(int i=0;i<A;i++) {
+			String str=in.next();
+			System.out.println(VPS(str));
+		}
+	}
+	public static String VPS(String str) {
+		Stack<Character> stack= new Stack<>();
+
+		for(int i=0;i<str.length();i++) {
+			char test=str.charAt(i);
+			if(test=='(') {
+				stack.push(test);
+			}
+			else if(stack.empty())
+				return "NO";
+			else stack.pop();
+		}
+		if(stack.empty())
+			return "yes";
+		else 
+			return "NO";
+	}
+}
