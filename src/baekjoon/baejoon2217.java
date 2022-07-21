571ce0d3111ae65423700ac4942cf7e5a5cbf92b
kangminlee1 <jack7853@naver.com>
2022-07-21 17:04:21 +0900

    baekjoon2217

--- src/baekjoon/baejoon2217.java
+++ src/baekjoon/baejoon2217.java
@@ -12,9 +12,9 @@ public class baejoon2217 {
 		for(int i=0;i<N;i++) {
 			arr[i]=in.nextInt();
 		}
-		Arrays.sort(arr);
+		Arrays.sort(arr);//정렬을 시킨후 뒤에서 부터 찾기
 		for(int i=N-1;i>=0;i--) {
-			arr[i]=arr[i]*(N-i);
+			arr[i]=arr[i]*(N-i);//무거운 순서이기 때문에 뒤에서부터 체크
 			if(max<arr[i])
 				max=arr[i];
 		}
