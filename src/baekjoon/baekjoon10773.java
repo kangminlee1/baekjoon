bc59e0da497d678fc423f434cf6ffe5956e56f14
kangminlee1 <jack7853@naver.com>
2023-06-21 19:37:08 +0900

    스택은 직접 구현한 방법으로 다시 푼것, 큐 구현한것

--- src/baekjoon/baekjoon10773.java
+++ src/baekjoon/baekjoon10773.java
@@ -1,27 +1,60 @@
 package baekjoon;
-import java.util.Stack;
+//import java.util.Stack;
 import java.util.Scanner;
 public class baekjoon10773 {
-
+	public static int top=-1;
+	public static int[] stack;
+	
+	static void push(int item) {
+		stack[++top]=item;
+	}
+	
+	static int pop() {
+		if(top==-1)
+			return -1;
+		else {
+			int item= stack[top];
+			stack[top]=0;
+			top--;
+			return item;
+		}
+	}
 
 	public static void main(String[] args) {
-		Stack<Integer> stack=new Stack<>();
+//		Stack<Integer> stack=new Stack<>();
 		Scanner in = new Scanner(System.in);
 		
 		int num=in.nextInt();
+//		
+//		for(int i=0;i<num;i++) {
+//			int a=in.nextInt();
+//			if(a==0)
+//				stack.pop();
+//			else 
+//				stack.push(a);
+//		}
+//		int sum=0;
+//		for(int i=0;i<stack.size();i++) {
+//			sum+=stack.get(i);
+//		}
+//
+//		System.out.println(sum);
 		
+		stack=new int[num];
 		for(int i=0;i<num;i++) {
-			int a=in.nextInt();
-			if(a==0)
-				stack.pop();
-			else 
-				stack.push(a);
+			int A=in.nextInt();
+			if(A==0) {
+				pop();
+			}
+			else {
+				push(A);
+			}
 		}
+		
 		int sum=0;
-		for(int i=0;i<stack.size();i++) {
-			sum+=stack.get(i);
+		for(int i=0;i<stack.length;i++) {
+			sum+=stack[i];
 		}
-
 		System.out.println(sum);
 	}
 
