36e3d181e32e53b72bbd3e3aa811f576aa28306e
kangminlee1 <jack7853@naver.com>
2023-06-21 17:40:10 +0900

    퀵정렬로 해결

--- src/baekjoon/baekjoon11004.java
+++ src/baekjoon/baekjoon11004.java
@@ -1,9 +1,38 @@
 package baekjoon;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class baekjoon11004 {
+	
+	static void quickSorting(int[] arr, int start, int end) {
+		int L=start;
+		int R=end;
+		int temp;
+		int pivot=arr[(start+end)/2];
+		
+		while(L<=R) {
+			while(arr[L]<pivot) {
+				L++;
+			}
+			while(arr[R]>pivot)
+				R--;
+		
+		if( L<=R) {
+			if(L!=R) {
+				temp=arr[L];
+				arr[L]=arr[R];
+				arr[R]=temp;
+			}
+			L++;
+			R--;
+		}
+		
+	 }
+		if(start<R)
+			quickSorting(arr, start, R);
+		if(L<end)
+			quickSorting(arr, L, end);
+	}
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -15,11 +44,9 @@ public class baekjoon11004 {
 		for(int i=0;i<arr.length;i++)
 			arr[i]=in.nextInt();
 		
-		Arrays.sort(arr);
-		
+		quickSorting(arr, 0 , A-1);
 		
 		System.out.println(arr[B-1]);
-		
 	}
 
 }
