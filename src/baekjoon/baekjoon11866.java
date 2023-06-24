dd92ecae10f3831c7cd1854f45f6f01c4d2f3984
kangminlee1 <jack7853@naver.com>
2023-06-24 17:57:20 +0900

    que 구현 문제는 que를 사용하는 문제로 바꿔서 풀어봄

---
+++ src/baekjoon/baekjoon11866.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class baekjoon11866 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		Queue<Integer> que = new LinkedList<>();
+		
+		int A=in.nextInt();
+		int B=in.nextInt();
+		
+		for(int i=1;i<=A;i++) {
+			que.offer(i);
+		}
+		System.out.print("<");
+		
+		while(que.size()>1) {//마지막 원소만 남을때 까지 반복
+			for(int i=0;i<B-1;i++) {
+				int val=que.poll();
+				que.offer(val);
+			}//3번째 원소 출력을 위해 1번째, 2번째 원소 뒤로 보냄
+			System.out.print(que.poll()+", ");//3번째 원소 출력
+			
+		}
+		System.out.println(que.poll()+">");//마지막 원소 출력
+		
+		
+	}
+
+}
--- src/baekjoon/baekjoon18258.java
+++ src/baekjoon/baekjoon18258.java
@@ -1,69 +1,43 @@
 package baekjoon;
 
 import java.util.Scanner;
-//import java.util.Queue;
-//import java.util.LinkedList;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class baekjoon18258 {
-	public static int[] queue;
-	public static int front=0;
-	public static int rear=0;
 	
-	static void push(int item) {
-		queue[rear]=item;
-		rear++;
-	}
 	
-	static int pop() {
-		if(rear==front)
-			return -1;
-		int res=queue[front];
-		front++;
-		return res;
-	}
-	
-	static int front() {
-		if(rear==front)
-			return -1;
-		
-		return queue[front];
-	}
-	
-	static int back() {
-		if(rear==front)
-			return -1;
-		
-		return queue[rear-1];
-	}
-	
-	static int empty() {
-		if(rear==front)
-			return 1;
-		else
-			return 0;
-	}
-	static int size() {
-		return rear-front;
-	}
 	
 	public static void main(String[] args) {
 		Scanner in =new Scanner(System.in);
-//		Queue<Integer> que= new LinkedList<>();
+		Deque<Integer> que= new LinkedList<>();
 		
 		int A=in.nextInt();
-		queue=new int[A];
+		
 		for(int i=0;i<A;i++) {
 			String str=in.next();
 			
-			switch (str){
-			case "push": push(in.nextInt()); break;
-			case "pop": System.out.println(pop()); break;
-			case "front": System.out.println(front()); break;
-			case "back": System.out.println(back()); break;
-			case "size": System.out.println(size());; break;
-			case "empty": System.out.println(empty());; break;
+			switch(str) {
+			case "push": que.offer(in.nextInt()); break;
+			case "front": System.out.println(que.peek());break;
+			case "back": System.out.println(que.peekLast());break;
+			case "size": System.out.println(que.size()); break;
+			case "empty": 
+				if(que.isEmpty())
+				System.out.println(1); 
+				else
+					System.out.println(0);
+				break;
+			case "pop": 
+				Integer item=que.pop();
+				if(item==null)
+				System.out.println(-1);
+				else
+					System.out.println(item);
+				break;
 			}
-
+			
+			
 		}
 		
 	}
