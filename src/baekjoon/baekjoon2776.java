a95460743aa7c0070ed8f495bf389c3002bdfbe7
kangminlee1 <jack7853@naver.com>
2023-07-04 20:31:13 +0900

    해시셋 사용 방법과 이진 탐색 사용 방법 모두 시간초과 발생 bufferReader Wrietter 사용 하는 방법 공부해야함

--- src/baekjoon/baekjoon2776.java
+++ src/baekjoon/baekjoon2776.java
@@ -2,50 +2,77 @@ package baekjoon;
 
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class baekjoon2776 {
 	
-	static int binarySearch(int[] arr, int n) {
-		int start= 0;
-		int last = arr.length-1;
-		while(start<=last) {
-			int mid=(start+last)/2;
-			
-			if(n<arr[mid]) {
-				last=mid-1;
-			}
-			else if(n>arr[mid]) {
-				start=mid+1;
-			}
-			else 
-				return 1;
-			
-		}
-		return 0;
-		
-	}
 
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		int T=in.nextInt();
 		
+		//HashSet 사용 방법
 		while(T-->0) {
-		int N=in.nextInt();
-		int[] arrN=new int[N];
-		for(int i=0;i<N;i++) {
-			arrN[i]=in.nextInt();
+			HashSet<Integer> hs =new HashSet<>();
+			int N=in.nextInt();
+			for(int i=0;i<N;i++) {
+				hs.add(in.nextInt());
+			}
+			int M=in.nextInt();
+			for(int i=0;i<M;i++) {
+				if(hs.contains(in.nextInt()))
+					System.out.println(1);
+				else
+					System.out.println(0);
+			}
+			
 		}
-		
-		Arrays.sort(arrN);
-		
-		int M=in.nextInt();
-		for(int i=0; i<M;i++) {
-			System.out.println(binarySearch(arrN, in.nextInt()));
-		}
-		}
-		
 		
 
 	}
 
 }
+
+//이진 탐색 사용 방법
+//static int binarySearch(int[] arr, int n) {
+//	int start= 0;
+//	int last = arr.length-1;
+//	while(start<=last) {
+//		int mid=(start+last)/2;
+//		
+//		if(n<arr[mid]) {
+//			last=mid-1;
+//		}
+//		else if(n>arr[mid]) {
+//			start=mid+1;
+//		}
+//		else 
+//			return 1;
+//		
+//	}
+//	return 0;
+//	
+//}
+//
+//public static void main(String[] args) {
+//	Scanner in=new Scanner(System.in);
+//	int T=in.nextInt();
+//	
+//	while(T-->0) {
+//	int N=in.nextInt();
+//	int[] arrN=new int[N];
+//	for(int i=0;i<N;i++) {
+//		arrN[i]=in.nextInt();
+//	}
+//	
+//	Arrays.sort(arrN);
+//	
+//	int M=in.nextInt();
+//	for(int i=0; i<M;i++) {
+//		System.out.println(binarySearch(arrN, in.nextInt()));
+//	}
+//	}
+//	
+//	
+//
+//}
