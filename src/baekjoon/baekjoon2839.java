8499f43eb9c6070778607ed517cdb0da45b66d50
kangminlee1 <jack7853@naver.com>
2023-09-08 18:07:35 +0900

    내가 생각했던 경우의 수에서 1가지 빠진것을 찾지 못해 입력 조건은 만족했지만 그 외에서 틀린것이 있는지 틀렸습니다가 나와 다른 사람이 푼 코드를 보고 부족한 점을 보완하였다.

---
+++ src/baekjoon/baekjoon2839.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class baekjoon2839 {
+	
+	public static void findMin(int N) {
+		
+		if(N==4||N==7)
+            System.out.println(-1);
+        else if(N%5==0)
+            System.out.println(N/5);
+        else if(N%5==1||N%5==3)
+            System.out.println(N/5+1);
+        else if(N%5==2||N%5==4)
+            System.out.println(N/5+2);
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		findMin(N);
+		
+	}
+
+}
