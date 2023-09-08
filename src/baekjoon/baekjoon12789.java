2464656da24450c96eaa3c1994e4a33883ace03e
kangminlee1 <jack7853@naver.com>
2023-09-08 19:54:17 +0900

    시도하다 실패하여 다른 문제 풀었음

---
+++ src/baekjoon/baekjoon12789.java
@@ -0,0 +1,60 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class baekjoon12789 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Stack<Integer> stack = new Stack<>();
+		Queue<Integer> que = new LinkedList<>();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i=0;i<N;i++) {
+			que.add(Integer.parseInt(st.nextToken()));
+		}
+				
+		int count = 1;
+		while(!que.isEmpty()) {
+				if(que.peek() == count) {
+					que.poll();
+					count++;
+				}
+				else if(!stack.isEmpty() && stack.peek() == count) {
+					stack.pop();
+					count++;
+				}
+				else {
+					stack.push(que.poll());
+			}
+			
+		}
+		
+		while(!stack.isEmpty()) {
+			if(stack.peek() == count) {
+				stack.pop();
+				count++;
+			}
+			else {
+				System.out.println("Sad");
+				return;
+			}
+			
+		}
+		System.out.println("Nice");
+		
+		
+	}
+
+}
---
+++ src/baekjoon/baekjoon2346.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class baekjoon2346 {
+
+	public static void main(String[] args) throws IOException {
+		//실패 다음에 다시
+		
+	}
+
+}
