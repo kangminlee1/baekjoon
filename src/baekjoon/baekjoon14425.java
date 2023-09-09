e5ebcec13db4f531941310ffef939939ce8e6e29
kangminlee1 <jack7853@naver.com>
2023-09-09 15:20:24 +0900

    scanner로 풀었을 때와 다르게 풀어 속도가 향상됨 //hashSet 잊지 않게 다시 사용해봄

--- src/baekjoon/baekjoon14425.java
+++ src/baekjoon/baekjoon14425.java
@@ -1,12 +1,42 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 import java.util.HashSet;
 
 public class baekjoon14425 {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+				
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		HashSet<String> set = new HashSet<>();
+		
+		for(int i=0;i<N;i++) {
+			set.add(br.readLine());
+		}
+		
+		int count = 0;
+		for(int i=0;i<M;i++) {
+			if(set.contains(br.readLine())) {
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+				
+	}
+}
+
+/*
+ * 		Scanner in = new Scanner(System.in);
 		HashSet<String> hs = new HashSet<>();
 		
 		int M=in.nextInt();
@@ -26,5 +56,4 @@ public class baekjoon14425 {
 		System.out.println(count);
 		
 	}
-
-}
+ */
