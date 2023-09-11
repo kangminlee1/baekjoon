d44101a7e02fa8ad6ebf5d397cbd3d318e508659
kangminlee1 <jack7853@naver.com>
2023-09-11 15:20:48 +0900

    다른 사람의 코드를 참조해봤지만 실패

--- src/baekjoon/baekjoon2346.java
+++ src/baekjoon/baekjoon2346.java
@@ -11,7 +11,42 @@ import java.util.ArrayDeque;
 public class baekjoon2346 {
 
 	public static void main(String[] args) throws IOException {
-		//실패 다음에 다시
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		Deque<int[]> deq = new ArrayDeque<>();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());		
+		for(int i=0;i<N;i++) {
+            int[] arr= {i+1,Integer.parseInt(st.nextToken())};
+			deq.add(arr);
+		}
+		
+		
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(deq.size()>1) {
+			int[] arr = deq.pollFirst();
+			sb.append(arr[0]).append(" ");
+			int count = arr[1];
+			
+			if(count>0) {
+				for(int i=1;i<count;i++) {
+					deq.add(deq.pollFirst());
+				}
+			}
+			else if(count<0) {
+				for(int i=1; i<-count;i++) {
+					deq.addFirst(deq.pollLast());
+				}
+			}
+		}
+		sb.append(deq.poll()[0]);
+		
+		System.out.println(sb);//실패함
 		
 	}
 
