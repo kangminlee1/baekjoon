5b20191519944a5589eaab054d5e7f58c1a63dd5
kangminlee1 <jack7853@naver.com>
2023-09-13 16:36:57 +0900

    수정 본

--- src/baekjoon/baekjoon10815.java
+++ src/baekjoon/baekjoon10815.java
@@ -1,30 +1,124 @@
 package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Scanner;
+
+public class baekjoon10815 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0;i<N;i++) {
+			int num= Integer.parseInt(st.nextToken());
+			map.put(i+1, num);
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<M;i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			if(map.containsValue(num)) {
+				sb.append(1).append(" ");
+			}else {
+				sb.append(0).append(" ");
+			}
+		}
+		System.out.println(sb);
+	}
+}
+
+/* scanner X
+ * import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class baekjoon10815 {
 	
-
-	public static void main(String[] args) {
-		Scanner in= new Scanner(System.in);
-		HashMap<Integer, Integer> hm=new HashMap<>();
+	public static int binarySearch(int[] arr, int N) {
+		int start = 0;
+		int last = arr.length;
 		
-		int A=in.nextInt();
-		int[] arr=new int[A];
-		for(int i=0;i<A;i++) {
-			arr[i]=in.nextInt();
-			hm.put(i+1, arr[i]);
-		}
-		int B=in.nextInt();
-		for(int i=0;i<B;i++) {
-			if(hm.containsValue(in.nextInt()))
-			System.out.print(1+" ");
-			else
-				System.out.print(0+" ");
+		while(start <= last) {
+			int mid = (start + last)/2;
+			
+			if( arr[mid]> N) {
+				last = mid -1;
+			}
+			else if(arr[mid] < N) {
+				start = mid + 1;
+			}
+			else {//찾았을때
+				return 1;
+			}
+			
 		}
 		
+		return 0;
 	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i= 0;i< N;i++) {
+			arr[i]= Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<M;i++) {
+			int number = Integer.parseInt(st.nextToken());
+			sb.append(binarySearch(arr, number)).append(" ");
+		}
+		System.out.println(sb);
+	}
+ */
+
+/*시간 초과 발생 다시 풀기
+Scanner in= new Scanner(System.in);
+HashMap<Integer, Integer> hm=new HashMap<>();
+
+int A=in.nextInt();
+int[] arr=new int[A];
+for(int i=0;i<A;i++) {
+	arr[i]=in.nextInt();
+	hm.put(i+1, arr[i]);
+}
+int B=in.nextInt();
+for(int i=0;i<B;i++) {
+	if(hm.containsValue(in.nextInt()))
+	System.out.print(1+" ");
+	else
+		System.out.print(0+" ");
+}
 
 }
+*/
+
+
 //이건 이진 탐색으로 해결
 //package baekjoon;
 //import java.util.Arrays;
