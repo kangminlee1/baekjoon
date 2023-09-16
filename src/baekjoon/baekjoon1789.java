9b43a1d207fdbb808d793de66f271450dafcd278
kangminlee1 <jack7853@naver.com>
2023-09-16 15:22:19 +0900

    풀었는데 초과했을 떄 조건을 생각하지 못하여 처음에 원하던 값에 +1 이 나온 값이 나오게 됨

--- src/baekjoon/baekjoon1789.java
+++ src/baekjoon/baekjoon1789.java
@@ -1,11 +1,47 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class baekjoon1789 {
 
-	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long S = Long.parseLong(br.readLine());
+		long max = 0;//1부터 더해나가서 sum == S가 되게 더할 값들
+		long sum = 0;
+
+		int i = 1;
+		while(sum<=S) {
+				sum += i++;
+				max++;
+		}
+		
+		if(sum == S) {
+			System.out.println(max);
+		}else if(sum > S) {
+			System.out.println(max - 1);
+		}
+	}
+}
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+	/*
+	
+			Scanner in=new Scanner(System.in);
 		
 		long num=in.nextLong();
 		long sum=0;
@@ -23,5 +59,4 @@ public class baekjoon1789 {
 		else if(sum>num)
 			System.out.println(count-1);//초과했을경우는 1개를 빼줘야 같음
 	}
-
-}
+	*/
