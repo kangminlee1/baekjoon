cf051b1ddadaab2f0c28a544684ba235c0fd02ba
kangminlee1 <jack7853@naver.com>
2023-09-18 16:41:29 +0900

    다시 풀어봄

--- src/baekjoon/baekjoon10845.java
+++ src/baekjoon/baekjoon10845.java
@@ -1,7 +1,108 @@
 package baekjoon;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class baekjoon10845 {
-	public static int[] queue;
+	
+	public static StringBuilder sb = new StringBuilder();
+	public static Queue<Integer> que = new LinkedList<>();
+	public static int back = 0;
+	
+	public static void push(int num) {
+		que.add(num);
+		back = num;
+	}
+	
+	public static int pop() {
+		if(que.isEmpty()) {
+			return -1;
+		}
+		int result = que.poll();
+		
+		return result;
+	}
+	
+	public static int size() {
+		int result = que.size();
+		
+		return result;
+	}
+	
+	public static int empty() {
+		if(que.isEmpty()) {
+			return 1;
+		}else {
+			return 0;
+		}
+	}
+	
+	public static int front () {
+		if(que.isEmpty()) {
+			return -1;
+		}else {
+			return que.peek();
+		}
+	}
+	
+	public static int back() {
+		if(que.isEmpty()) {
+			return -1;
+		}else {
+			return back;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		for(int i=0; i< N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			String str = st.nextToken();
+			
+			if(str.equals("push")) {
+				int num = Integer.parseInt(st.nextToken());
+				push(num);
+			}
+			if(str.equals("pop")) {
+				sb.append(pop()).append("\n");
+				
+			}
+			if(str.equals("size")) {
+				sb.append(size()).append("\n");
+			}
+			if(str.equals("empty")) {
+				sb.append(empty()).append("\n");
+			}
+			if(str.equals("front")) {
+				sb.append(front()).append("\n");
+			}
+			if(str.equals("back")) {
+				sb.append(back()).append("\n");
+			}
+			
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+}
+
+
+/*
+ * 
+ * public static int[] queue;
 	public static int rear=0;
 	public static int front=0;
 	
@@ -40,36 +141,36 @@ public class baekjoon10845 {
 		else
 			return queue[rear-1];
 	}
+ * 
+ */
 
-	public static void main(String[] args) {
-		Scanner in= new Scanner(System.in);
-		int A=in.nextInt();
-		
-		queue=new int[A];
-		for(int i=0;i<A;i++) {
-			String str=in.next();
-			
-			switch(str) {
-			case "push": 
-				push(in.nextInt());
-				break;
-			case "pop":
-				System.out.println(pop());
-				break;
-			case "size":
-				System.out.println(size());
-				break;
-			case "empty":
-				System.out.println(empty());
-				break;
-			case "front":
-				System.out.println(front());
-				break;
-			case "back":
-				System.out.println(back());
-				break;
-			}
-		}
+/*
+Scanner in= new Scanner(System.in);
+int A=in.nextInt();
+
+queue=new int[A];
+for(int i=0;i<A;i++) {
+	String str=in.next();
+	
+	switch(str) {
+	case "push": 
+		push(in.nextInt());
+		break;
+	case "pop":
+		System.out.println(pop());
+		break;
+	case "size":
+		System.out.println(size());
+		break;
+	case "empty":
+		System.out.println(empty());
+		break;
+	case "front":
+		System.out.println(front());
+		break;
+	case "back":
+		System.out.println(back());
+		break;
 	}
-
 }
+}*/
--- src/baekjoon/baekjoon10866.java
+++ src/baekjoon/baekjoon10866.java
@@ -1,10 +1,106 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class baekjoon10866 {
 	
-	static int front=0;
+	public static Deque<Integer> deq = new ArrayDeque<>();
+	
+	
+	public static void push_front(int num) {
+		deq.addFirst(num);
+	}
+	public static void push_back(int num) {
+		deq.addLast(num);
+	}
+	public static int pop_front() {
+		if(deq.isEmpty()) {
+			return -1;
+		}
+		return deq.removeFirst();
+	}
+	public static int pop_back() {
+		if(deq.isEmpty()) {
+			return -1;
+		}
+		return deq.removeLast();
+	}
+	public static int size() {
+		return deq.size();
+	}
+	public static int empty() {
+		if(deq.isEmpty()) {
+			return 1;
+		}else
+			return 0;
+	}
+	public static int front() {
+		if(deq.isEmpty()) {
+			return -1;
+		}
+		return deq.peekFirst();
+	}
+	public static int back() {
+		if(deq.isEmpty()) {
+			return -1;
+		}
+		return deq.peekLast();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i =0;i < N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			String str = st.nextToken();
+			
+			if(str.equals("push_back")) {
+				int num = Integer.parseInt(st.nextToken());
+				push_back(num);
+			}
+			if(str.equals("push_front")) {
+				int num = Integer.parseInt(st.nextToken());
+				push_front(num);
+			}
+			if(str.equals("pop_back")) {
+				sb.append(pop_back()).append("\n");
+			}
+			if(str.equals("pop_front")) {
+				sb.append(pop_front()).append("\n");
+			}
+			if(str.equals("size")) {
+				sb.append(size()).append("\n");
+			}
+			if(str.equals("empty")) {
+				sb.append(empty()).append("\n");
+			}
+			if(str.equals("front")) {
+				sb.append(front()).append("\n");
+			}
+			if(str.equals("back")) {
+				sb.append(back()).append("\n");
+			}
+		}
+		System.out.println(sb);
+
+	}
+
+}
+
+
+
+/*
+ * static int front=0;
 	static int rear=0;
 	static int size=0;
 	static int[] arr;
@@ -100,3 +196,5 @@ public class baekjoon10866 {
 	}
 
 }
+ * 
+ */
