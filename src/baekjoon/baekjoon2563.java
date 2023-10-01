01172994b414c305eb785af12044da3af6e912ba
kangminlee1 <jack7853@naver.com>
2023-10-01 17:24:19 +0900

    나는 각각 넓이가 따로 있을 경우인 300에서 겹치는 부분을 빼는 방식으로 생각했는데 경우가 너무 많아 찾아보니 더 간단한 방법 발견

--- src/baekjoon/baekjoon2563.java
+++ src/baekjoon/baekjoon2563.java
@@ -1,10 +1,50 @@
 package baekjoon;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
 
 public class baekjoon2563 {
 	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static final int x = 100;
+	public static final int y = 100;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				
+		int N = Integer.parseInt(br.readLine());
+
+		boolean[][] TF = new boolean[x][y];
+		
+		int area = 0;
+		for(int i =0; i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int point_x = Integer.parseInt(st.nextToken());
+			int point_y = Integer.parseInt(st.nextToken());
+			
+			for(int j = point_x; j<point_x+10; j++) {
+				for(int k= point_y; k<point_y+10; k++) {
+					if(!TF[j][k]) {
+						TF[j][k]=true;
+						area++;
+					}
+				}
+			}
+		}
+		
+		System.out.println(area);
+		
+		
+	}
+
+}
+
+
+/*
+Scanner in = new Scanner(System.in);
 		
 		int total=0;
 		int num = in.nextInt();
@@ -30,7 +70,5 @@ public class baekjoon2563 {
 		}
 		
 		System.out.println(total);
-		
-	}
 
-}
+*/
