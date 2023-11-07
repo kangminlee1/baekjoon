f422abdb92a08fc7dec8ef5aad58a91870efce7f
kangminlee1 <jack7853@naver.com>
2023-11-07 21:15:18 +0900

    실패했던 문제 해결

--- src/baekjoon/baekjoon1049.java
+++ src/baekjoon/baekjoon1049.java
@@ -10,24 +10,6 @@ public class baekjoon1049 {
 	
 	public static int[][] arr;
 	
-	public static int counting(int N) {
-		int price=0;
-		int count = 0;
-		
-		while(true) {
-			
-			if(N <= count) {
-				break;
-			}
-			
-			
-			//다시 생각
-			
-			
-		}
-		
-		return 0;
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,9 +27,41 @@ public class baekjoon1049 {
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
+		int group_min = Integer.MAX_VALUE;
+		int min = Integer.MAX_VALUE;
+		int total = 0;
 		
+		for(int i =0; i<M; i++) {
+			if(arr[i][0]<group_min) {
+				group_min = arr[i][0];
+			}
+			if(arr[i][1]<min) {
+				min = arr[i][1];
+			}
+		}
 		
-
+		while(0 < N) {
+			if(group_min/6 < min) {
+				if(N<6) {
+					if(group_min < min*N) {
+						total+=group_min;
+						N-=6;
+					}else {
+						total+=min*N;
+						N-=N;
+					}
+					
+				}else {
+					total+=group_min;
+					N-=6;
+				}
+			}else{
+				total+=N*min;
+				N-=N;
+			}
+		}
+		
+		System.out.println(total);
 	}
 
 }
