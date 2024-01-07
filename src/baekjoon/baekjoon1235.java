74e9b8885ce9517c862f490b59777173956f0128
kangminlee1 <jack7853@naver.com>
2024-01-07 16:58:04 +0900

    set을 사용하는 것을 생각하지 못하여 쓸데 없는 코드 들이 생기게 되었음 -> 해결

---
+++ src/baekjoon/baekjoon1235.java
@@ -0,0 +1,39 @@
+package baekjoon;
+//학생번호
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class baekjoon1235 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] list = new String[N];
+		Set<String> set = new HashSet<>();
+		
+		for(int i =0; i<N; i++) {
+			list[i] = br.readLine();
+		}
+		int sl = list[0].length(); //문자열의 길이
+		
+		for(int i = 1; i<=sl; i++) {
+			for(int j = 0; j<list.length; j++) {
+				set.add(list[j].substring(sl-i));
+			}
+			if(set.size() == N) {//set은 중복을 허용하지 않기 때문에
+				//원소 수와 학생 수가 같아질 때 값을 구한다 -> 학생 번호를 서로 다르게 만들 수 있는 최소의 수
+				System.out.println(i);
+				return; //종료
+			}
+			set.clear();//초기화 시켜주면서 해야 원소 수와 학생 수가 같아질 때를 구할 수 있음
+		}
+		
+
+	}
+
+}
---
+++ src/baekjoon/baekjoon15624.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class baekjoon15624 {
+
+	public static int[] dp;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		dp = new int[1000001];
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		for(int i = 2; i<=n; i++) {
+			dp[i] = (dp[i-2] + dp[i-1]) % 1000000007;
+			//n이 커지면 피보나치 수가 너무 커지니까 나머지로 구해서 수를 int 범위를 벗어나지 않게 하는 것
+		}
+
+		System.out.println(dp[n]);
+	}
+
+}
