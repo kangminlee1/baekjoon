18fe30b0917772e09c9fc9a833516d0dbe64c809
kangminlee1 <jack7853@naver.com>
2024-01-18 17:22:55 +0900

    백준 문제 풀이 solved.ac 문제 모음 풀기 시작

--- src/baekjoon/baekjoon1021.java
+++ src/baekjoon/baekjoon1021.java
@@ -6,63 +6,82 @@ import java.io.IOException;
 
 import java.util.StringTokenizer;
 
-//import java.util.Deque;
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class baekjoon1021 {
 
-//	public static Deque<Integer> dq = new LinkedList<>();
-	public static LinkedList<Integer> que = new LinkedList<>();
+	public static Deque<Integer> dq = new LinkedList<>();
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		LinkedList<Integer> deque = new LinkedList<Integer>();
 		
-		int[] arr = new int[M];
+		int count = 0;	// 2, 3번 연산 횟수 누적 합 변수
 		
-		for(int i =1; i<=N;i++) {
-			que.add(i);
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());	// 큐의 크기(1 ~ N)
+		int M = Integer.parseInt(st.nextToken());	// 뽑으려는 숫자의 개수
+		
+		// 1부터 N까지 덱에 담아둔다.
+		for(int i = 1; i <= N; i++) {
+			deque.offer(i);
 		}
 		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<M; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		int[] seq = new int[M];	// 뽑고자 하는 수를 담은 배열
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < M; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int count = 0;
 		
-		for(int i = 0; i<M ; i++) {
-			int mid = 0;
-			int target = que.indexOf(arr[i]);//위치 찾기 찾으면 인덱스값 못찾으면 -1
-			//target에 값은 0~size-1까지
+		for(int i = 0; i < M; i++) {
 			
-			if(que.size()%2 == 0) {//인덱스 기준으로 봐야함 -> -1 하는 이유
-				mid = que.size()/2 -1;
-			}else{
-				mid = que.size()/2;
+			
+			// 덱에서 뽑고자 하는 숫자의 위치(index) 찾기 
+			int target_idx = deque.indexOf(seq[i]);
+			int half_idx;
+			/*
+			 *  만약 현재 덱의 원소가 짝수 개라면 중간 지점을 
+			 *  현재 덱의 절반 크기에서 -1 감소시킨다. 
+			 *  
+			 *  {1, 2, 3, 4} 일 때, 2를 중간지점으로 하면
+			 *  인덱스는 1이기 때문
+			 */
+			if(deque.size() % 2 == 0) {
+				half_idx = deque.size() / 2 - 1;
+			}
+			else {
+				half_idx = deque.size() / 2;
 			}
 			
-			if(target<=mid) {//찾고자 하는 것이 중앙보다 작을 경우
-				for(int j = 0; j<target; j++) {
-					que.addLast(que.pollFirst());//앞에서 빼서 뒤로 보냄
-					count++;
-				}
-				
-			}else{//중앙보다 크면
-				for(int j = 0; j<que.size() - target;j++){
-					que.addFirst(que.pollLast());//뒤에서 뺴서 앞으로 보냄
+			
+			// 중간 지점 또는 중간 지점보다 원소의 위치가 앞에 있을 경우
+			if(target_idx <= half_idx) {
+				// idx 보다 앞에 있는 원소들을 모두 뒤로 보낸다. (2번 연산)
+				for(int j = 0; j < target_idx; j++) {
+					int temp = deque.pollFirst();
+					deque.offerLast(temp);
 					count++;
 				}
 			}
+			else {	// 중간 지점보다 원소의 위치가 뒤에 있는 경우 
+				// idx를 포함한 뒤에 있는 원소들을 모두 앞으로 보낸다. (3번 연산)
+				for(int j = 0; j < deque.size() - target_idx; j++) {
+					int temp = deque.pollLast();
+					deque.offerFirst(temp);
+					count++;
+				}
 			
-			que.poll();
+			}
+			deque.pollFirst();	// 연산이 끝나면 맨 앞 원소를 삭제
 		}
 		
 		System.out.println(count);
 		
+		
 	}
-
 }
--- src/baekjoon/baekjoon11723.java
+++ src/baekjoon/baekjoon11723.java
@@ -6,77 +6,48 @@ import java.io.IOException;
 
 import java.util.StringTokenizer;
 import java.util.HashSet;
-//실패 비스마스킹 사용문제임
+//실패했었음 비스마스킹 사용문제임
 public class baekjoon11723 {
 	
 	public static HashSet<Integer> set = new HashSet<>();
 	
-	public static void add(int x) {
-		if(set.contains(x)) {
-			return;
-		}
-		set.add(x);
-	}
-	
-	public static void remove(int x) {
-		if(set.contains(x)) {
-			set.remove(x);
-		}
-	}
-	public static int check(int x) {
-		if(set.contains(x)) {
-			return 1;
-		}
-		
-		return 0;
-	}
-	public static void toggle(int x) {
-		if(set.contains(x)) {
-			set.remove(x);
-		}
-		set.add(x);
-	}
-	public static void all() {
-		set.clear();
-		for(int i =1; i<=20; i++) {
-			set.add(i);
-		}
-	}
-	public static void empty() {
-		set.clear();
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
+		StringBuilder sb = new StringBuilder();
+		int S = 0;//집합
 		int M = Integer.parseInt(br.readLine());
 		
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i =0; i<M;i++) {
-			String str = st.nextToken();
-			
-			if(str.equals("add")) {
-				add(Integer.parseInt(br.readLine()));
-			}
-			else if(str.equals("check")) {
-				sb.append(check(Integer.parseInt(br.readLine()))).append("\n") ;
-			}
-			else if(str.equals("remove")) {
-				remove(Integer.parseInt(br.readLine()));
-			}
-			else if(str.equals("toggle")) {
-				toggle(Integer.parseInt(br.readLine()));
-			}
-			else if(str.equals("all")) {
-				all();
-			}
-			else if(str.equals("empty")) {
-				empty();
-			}
-		}
+		StringTokenizer st;
 		
-		System.out.println(sb);
+		for(int i = 0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			String str = st.nextToken();
+			if(str.equals("all"))
+				S =(1<<21) -1 ;
+			//all이면 S를 1~20으로 바꿈
+			else if(str.equals("empty"))
+				S = 0;
+			else {
+				int num = Integer.parseInt(st.nextToken());
+				
+				if(str.equals("add"))
+					S |= (1<<num);
+				
+				else if(str.equals("remove"))
+					S &= ~(1<<num);
+				
+				else if(str.equals("toggle"))
+					S ^= (1<<num);
+				
+				else if(str.equals("check"))
+					sb.append((S & (1<<num)) != 0 ? 1:0).append("\n");			
+			}
+			
+		}
+			
+			System.out.println(sb);
 		
 	}
 
