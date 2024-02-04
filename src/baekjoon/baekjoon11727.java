ae1a5dbb3f5f76cc9b5baf38385e3d991d37ff22
kangminlee1 <jack7853@naver.com>
2024-02-04 20:58:17 +0900

    %10007의 조건을 보지 않았어서 수정

--- src/baekjoon/baekjoon11727.java
+++ src/baekjoon/baekjoon11727.java
@@ -1,37 +1,35 @@
 package baekjoon;
-//2*n 타일링
-//파도반 수열이랑 같은 문제임
+//2*N 타일링 2
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class baekjoon11726 {
-
-	public static Long[] dp = new Long[1001];
+public class baekjoon11727 {
 	
+	public static Long[] dp = new Long[1001];
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		
 		dp[1] = 1L;
-		dp[2] = 2L;
-		dp[3] = 3L;
+		dp[2] = 3L;
+		dp[3] = 5L;
 		
 		System.out.println(tile(N));
+		
+		
 	}
 	
 	public static long tile(int N) {
 		
 		if(dp[N] == null) {
-			dp[N] = (tile(N-2) + tile(N-1))%10007;
+			dp[N] = (tile(N-1) + 2*tile(N-2))%10007;
+			
 		}
-		
 		
 		return dp[N];
 	}
-	
-	
 
 }
